a85038b9defa382936c4b97b7cbb506740866253
alexi <alexis.kueny@outlook.com>
2024-12-05 13:45:49 +0100

    Commit

--- ISChess-master/ISChess-master/Bots/BaseChessBot.py
+++ ISChess-master/ISChess-master/Bots/BaseChessBot.py
@@ -41,22 +41,30 @@ def pawn_mover_bot(player_sequence: str, board, time_budget, **kwargs):
     print(board)
 
     # Populating dictionary with pieces
+    count = 0
     for y in range(board.shape[0]):
         for x in range(board.shape[1]):
             print(str(board[y][x]))
             if color in str(board[y][x]):
-                piece_moves[str(board[y][x])] = [(y, x)]
+                piece_moves[str(board[y][x])+str(count)] = [(y, x)]
+                count += 1
     print(piece_moves)
 
     # Matching each piece with possible moves
+
     for piece in piece_moves:
         for y in range(board.shape[0] - 1):
             for x in range(board.shape[1]):
                 start = piece_moves[piece][0]
                 end = (y, x)
                 move = (start, end)
-                if ChessRules.move_is_valid(player_sequence, move, board):
-                    piece_moves[piece].append(move[1])
+                print(move)
+                try:
+                    if ChessRules.move_is_valid(player_sequence, move, board):
+                        piece_moves[piece].append(move[1])
+                except IndexError:
+                    continue
+
     print(piece_moves)
 
     #find possible moves for each piece
